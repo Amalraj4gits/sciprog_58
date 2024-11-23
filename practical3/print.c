10d3bb07cd1e838bd7a8e3afd0b8a7d62f927dbd
Amalraj4gits <134486556+Amalraj4gits@users.noreply.github.com>
2024-11-23 12:55:07 +0000

    Add files via upload

---
+++ practical3/print.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+ 
+
+ int main() {
+
+int   i1=20;
+int   i2=50;
+float fl1=2.2222;
+float fl2=3.3333;
+printf("The value of integer 1 is %d\n", i1);
+printf("The value of integer 2 is %d\n",i2);
+printf("The value of integer 1 and 2 are %d,%d\n", i1 , i2);
+printf("The value of float 1 and float 2 are %f, %f\n", fl1 ,fl2);
+
+
+
+
+
+
+
+
+   return 0;
+ }   
+
